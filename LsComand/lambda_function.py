8c90ef6d4eb93ee5dc0576a00835dbf10dd00df4
tumugi2205 <tumugi2205@yahoo.co.jp>
2020-03-16 14:45:47 +0900

    一覧データを返すLambdafunction

---
+++ LsComand/lambda_function.py
@@ -0,0 +1,47 @@
+import json
+import os
+from os.path import join, dirname
+
+import boto3
+from dotenv import load_dotenv
+
+# command python-lambda-local -f lambda_handler lambda_function.py event.json
+
+def lambda_handler(event, context):
+    """lsコマンドが入力された時のlambda関数
+    
+    Arguments:
+        event {dict} -- lambda実行時に渡すデータ
+        context {dict} -- 正直わかってない。たぶん周りのファイルとかかな
+    
+    Returns:
+        json -- 取得したデータをjsonで返す
+    """
+    dynamodb = connect_db()
+    result_data = {}
+    user_name = event["user_name"]
+    command = event["command"]
+    action = event["action"]
+    task = event["task"]
+    table = dynamodb.Table('TodoCan')
+    user_data = table.get_item(Key={"user_name":user_name})["Item"]
+
+    if "todo" == action:
+        return user_data["TODO"]
+    elif "doing" == action:
+        return user_data["DOING"]
+    elif "done" == action:
+        return user_data["DONE"]
+
+
+def connect_db():
+    return boto3.resource('dynamodb',
+        region_name='us-east-2',
+        aws_access_key_id=get_key("aws_access_key_id"),
+        aws_secret_access_key=get_key("aws_secret_access_key") 
+        )
+        
+def get_key(key_name):
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    return os.environ.get(key_name)
